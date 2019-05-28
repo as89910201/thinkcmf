62ab38d34b477d6a2b6042d2cd8a6727cfe7aeb8
刘浩宇 <2083709400@qq.com>
2019-05-28 18:44:23 +0800

    My feature is ready

---
+++ data/config/database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+
+return [
+    // 数据库类型
+    'type'     => 'mysql',
+    // 服务器地址
+    'hostname' => '39.98.205.63',
+    // 数据库名
+    'database' => 'thinkcmf',
+    // 用户名
+    'username' => 'root',
+    // 密码
+    'password' => 'root',
+    // 端口
+    'hostport' => '3306',
+    // 数据库编码默认采用utf8
+    'charset'  => 'utf8',
+    // 数据库表前缀
+    'prefix'   => 'cmf_',
+    "authcode" => 'I2ki54j76javre9LJY',
+    //#COOKIE_PREFIX#
+];
